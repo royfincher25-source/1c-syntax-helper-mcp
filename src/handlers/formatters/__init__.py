9a374cfcee29582ed9b2db3f37c44d7df5fbf591
Roy Fincher <royfincher25@gmail.com>
2026-03-07 02:15:44 +0300

    refactor: удалить дублирующиеся formatters

--- src/handlers/formatters/__init__.py
+++
@@ -1,9 +0,0 @@
-from src.handlers.formatters.search_formatter import SearchFormatter
-from src.handlers.formatters.syntax_formatter import SyntaxFormatter
-from src.handlers.formatters.object_formatter import ObjectFormatter
-
-__all__ = [
-    'SearchFormatter',
-    'SyntaxFormatter',
-    'ObjectFormatter',
-]
